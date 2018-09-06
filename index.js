77dc294554b5ebcdb098cab2787870569fa1e64f
DianaAr8 <dianar_8_8otmail.com>
2018-09-06 09:59:12 -0500

    Agregando función para lectura y conteo de líneas del archivo (README.md)

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ const checkRoute = (file) => {
 
 // Se manda llamar la función para que se ejecute y como parámetro se le da la ruta del archivo 
 checkRoute('./README.md');
+
+// Se crea función para leer y contar las líneas del archivo
+// Se utiliza para leer archivos, se da la ruta y se utiliza "utf8" para reconocimiento de acentos en el archivo al ser leído 
+const countLines = fs.readFile('./README.md', 'utf-8', (err, file) => {
+  if (err) {
+    console.log('Error, no se encontro archivo');
+  } else {
+    console.log(file);
+    let lines = file.split('\n').length;
+    // Se retorna el resultado y se concatena para imprimir en consola el total de líneas que contiene el archivo
+    console.log(lines + ' ' + 'lines');
+    return lines + ' ' + 'lines';
+  }
+});
