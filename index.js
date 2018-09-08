6ecdd6a692bb5576b0ddd1b22c7f1558e17d8b78
DianaAr8 <dianar_8_8otmail.com>
2018-09-08 04:36:25 -0500

    Se da identación al código y se agregan

--- index.js
+++ index.js
@@ -14,34 +14,31 @@ const checkRoute = (file) => {
   // Si es relativa la cambiara a absoluta con el metodo path.resolve
   const convertRoute = path.resolve(file);  
 
-  fs.readFile( convertRoute, 'utf-8', (err, file) => {
+  fs.readFile(convertRoute, 'utf-8', (err, file) => {
     if (err) {
       console.log('Error, no se encontro archivo');
     } 
     console.log(convertRoute);
-      url(file);
-    
-});
- }
- checkRoute('./README.md');
+    url(file);
+  });
+};
+checkRoute('./README.md');
  
 
 const url = (file) => { 
   const expReg = /\[([^\[\]]*)\]\(((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,}))\)/g;
   let readLinks = file.match(expReg);
-  //console.log(readLinks);
+  // console.log(readLinks);
   let arrayLinks = readLinks.map((item) => {
     const arrayInfo = item.split(/\[([^[\]]*)\]\(([^()]*)\)/g);
-      let status = {
-        href: arrayInfo[2],
-        text: arrayInfo[1],
-        file:  convertRoute
-      }
-      return status;
-  })
+    let status = {
+      href: arrayInfo[2],
+      text: arrayInfo[1],
+      file: convertRoute
+    };
+    return status;
+  });
   console.log(arrayLinks);
   return arrayLinks;
-
-
 };
 
