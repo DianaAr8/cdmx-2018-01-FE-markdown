4693820970f8463107dfc45bd7aad46a4e8a9ab3
DianaAr8 <dianar_8_8otmail.com>
2018-09-08 04:28:11 -0500

    Agregando función con un arreglo de objetos que relorna (href, text y file

--- index.js
+++ index.js
@@ -5,35 +5,43 @@ const fs = require('fs');
 
 const fetch = require('node-fetch');
 
+let convertRoute;
+
 
 // Se crea una función para traer y verificar la ruta si es relativa o absoluta
 const checkRoute = (file) => {
   console.log(file);
   // Si es relativa la cambiara a absoluta con el metodo path.resolve
   const convertRoute = path.resolve(file);  
-  console.log(convertRoute);
-};
 
-// Se manda llamar la función para que se ejecute y como parámetro se le da la ruta del archivo 
-checkRoute('./README.md');
-
-// Se crea función para leer y contar las líneas del archivo
-// Se utiliza para leer archivos, se da la ruta y se utiliza "utf8" para reconocimiento de acentos en el archivo al ser leído 
-const countLines = fs.readFile('./README.md', 'utf-8', (err, file) => {
-  if (err) {
-    console.log('Error, no se encontro archivo');
-  } else {
-    // console.log(file);
-    url(file);
-    let lines = file.split('\n').length;
-    // Se retorna el resultado y se concatena para imprimir en consola el total de líneas que contiene el archivo
-    console.log(lines + ' ' + 'lines');
-    return lines + ' ' + 'lines';
-  };
+  fs.readFile( convertRoute, 'utf-8', (err, file) => {
+    if (err) {
+      console.log('Error, no se encontro archivo');
+    } 
+    console.log(convertRoute);
+      url(file);
+    
 });
+ }
+ checkRoute('./README.md');
+ 
 
 const url = (file) => { 
-  const expReg = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g;
-  const readLink = file.match(expReg);
-  console.log(readLink);
+  const expReg = /\[([^\[\]]*)\]\(((https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,}))\)/g;
+  let readLinks = file.match(expReg);
+  //console.log(readLinks);
+  let arrayLinks = readLinks.map((item) => {
+    const arrayInfo = item.split(/\[([^[\]]*)\]\(([^()]*)\)/g);
+      let status = {
+        href: arrayInfo[2],
+        text: arrayInfo[1],
+        file:  convertRoute
+      }
+      return status;
+  })
+  console.log(arrayLinks);
+  return arrayLinks;
+
+
 };
+
