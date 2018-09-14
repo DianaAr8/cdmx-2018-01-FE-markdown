f43bc561a8b2d9cef22724274a231fe9f8109a2b
DianaAr8 <dianar_8_8otmail.com>
2018-09-14 11:53:55 -0500

    Refactorizando código

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,12 +1,10 @@
-const { checkRoute, url, idLinks } = require('../index.js');
+const { checkRoute,searchLinks} = require('../index.js');
 
 describe('checkRoute', () => {
   test('Debería checkRoute ser una función', () => {
     expect(typeof checkRoute).toEqual('function');
   });
-  test('Debería url ser una función', () => {
-    expect(typeof url).toEqual('function');
+  test('Debería searchLinks ser una función', () => {
+    expect(typeof searchLinks).toEqual('function');
   });
 });
-
-
